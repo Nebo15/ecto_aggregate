a68c1d9f8462aeeeece049e0927985cb34218582
Andrew Dryga <andrew@dryga.com>
2017-04-12 19:08:55 +0300

    Drop unused files

--- bin/hooks/pre-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# `pwd` should be /opt/ecto_aggregate
-APP_NAME="ecto_aggregate"
-
-if [ "${DB_MIGRATE}" == "true" ]; then
-  echo "[WARNING] Migrating database!"
-  ./bin/$APP_NAME command "${APP_NAME}_tasks" migrate!
-fi;
